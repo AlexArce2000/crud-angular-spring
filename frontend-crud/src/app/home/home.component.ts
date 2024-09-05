299839f0f82e7d7c38d00b80c38b5ddfe7910835
AlexArce2000 <alexisroarce2000@gmail.com>
2024-09-04 23:22:54 -0400

    fix

---
+++ frontend-crud/src/app/home/home.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { Book } from '../models/book';
+import { BookService } from '../services/book.service';
+import { RouterModule } from '@angular/router';
+import { MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ButtonModule, CardModule, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  books:Book[]=[]
+  isDeleteInProgress: boolean = false
+  constructor(private bookService: BookService, private messageService: MessageService){}
+  
+  ngOnInit(): void {
+    this.getAllBooks();
+  }
+  getAllBooks(){
+    this.bookService.getBooks().subscribe(data=>{
+      this.books = data;
+
+    })
+  }
+  deleteBook(id:number){
+    this.isDeleteInProgress=true
+    this.bookService.deleteBook(id).subscribe({
+      next:()=>{
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Borrado',
+          detail: 'Libro Eliminado Correctamente',
+        });
+        this.isDeleteInProgress=false
+        this.getAllBooks()
+      },
+      error: () =>{
+        this.isDeleteInProgress=false 
+        this.messageService.add({
+          severity: 'error',
+          summary: 'error',
+          detail: 'No se pudo eliminar el libro',
+        });
+      }
+    })
+  }
+}
